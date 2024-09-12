e2abc9a033455c1b18d27a8e89dc85d3c5837ac3
bai ying <cn.baiying@gmail.com>
2024-09-12 11:21:09 +0800

    Update common.py

--- src/foundation_python/util/common.py
+++ src/foundation_python/util/common.py
@@ -32,5 +32,23 @@ def load_local_config():
     加载本地配置
     :return:
     """
-    print(f"sys.path: {sys.path[0]}")
+    project_root = sys.path[0]
+    local_config_file = os.path.abspath(os.path.join(project_root, 'local_config.yaml'))
+    if not os.path.exists(local_config_file):
+        raise FileNotFoundError('未找到本地配置文件 local_config.yaml，请创建该配置文件后再次尝试')
+    with open(local_config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def load_config():
+    """
+    加载全局配置和本地配置
+    :return:
+    """
+    global_config = load_global_config()
+    local_config = load_local_config()
+    return {**global_config, **local_config}
+
+
 
